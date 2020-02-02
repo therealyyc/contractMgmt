77729cf632d1d6e377ffa691e13f9ccc1c5cc270
yyc <donkeylordest@163.com>
2020-02-02 20:16:17 +0800

    delete node-modules

--- public/react-component/webpack.config.js
+++ public/react-component/webpack.config.js
@@ -2,10 +2,23 @@ const path = require('path')
 
 
 const config = {
+  mode: 'development',
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s(c|a)ss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      }
+    ]
   }
 }
 
